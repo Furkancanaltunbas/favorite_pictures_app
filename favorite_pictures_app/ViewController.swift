f28bf2474492e5135264a3f4f81748b1468ad242
FurkanCan <furkancan1342@gmail.com>
2022-09-09 13:35:26 +0300

    Initial Commit

---
+++ favorite_pictures_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  favorite_pictures_app
+//
+//  Created by FurkanCan on 09/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
